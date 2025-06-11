fae7ac875fb4d0bdfbd91ae69dae413f9767e409
Kayki Letieri <kaykiletieri37@gmail.com>
2025-06-11 00:49:58 -0300

    feat: add initial CreditCard entity with properties and constructor

---
+++ Domain/Entities/CreditCard.cs
@@ -0,0 +1,21 @@
+﻿namespace Domain.Entities;
+
+public sealed class CreditCard : Entity
+{
+    public string LastFourDigits { get; private set; }
+    public string CardHolderName { get; private set; }
+    public DateTime ExpirationDate { get; private set; }
+    public string? Description { get; private set; }
+    public bool IsActive { get; private set; }
+    public decimal? Limit { get; private set; }
+
+    public CreditCard(string lastFourDigits, string cardHolderName, DateTime expirationDate, bool isActive, decimal? limit = null, string? description = null)
+    {
+        LastFourDigits = lastFourDigits;
+        CardHolderName = cardHolderName;
+        ExpirationDate = expirationDate;
+        IsActive = isActive;
+        Limit = limit;
+        Description = description;
+    }
+}
