01606871cdf86029c3c1922e8cbda90f2d37db72
Kayki Letieri <kaykiletieri37@gmail.com>
2025-06-11 00:50:56 -0300

    feat: add Category property to Income entity

--- Domain/Entities/Income.cs
+++ Domain/Entities/Income.cs
@@ -1,10 +1,12 @@
-﻿using Domain.ValueObjects;
+﻿using Domain.Enums;
+using Domain.ValueObjects;
 
 namespace Domain.Entities;
 
 public class Income : Transaction
 {
     public string? Source { get; private set; }
+    public IncomeCategory Category { get; private set; }
 
     public Income(DateTime transactionDate, Money amount, string source, string? description = null)
         : base(transactionDate, amount, description)
